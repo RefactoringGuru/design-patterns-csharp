be6002c5698b6d3aad4177c93c2184f5f2e1812b
alexey55501 <33526118+alexey55501@users.noreply.github.com>
2018-06-01 21:01:58 +0300

    Add files via upload

---
+++ RefactoringGuru.DesignPatterns.Builder.Structural/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    class Program
+    {
+        /**
+ * The client code creates a builder object, passes it to the director and then
+ * initiates the construction process. The end result is retrieved from the
+ * builder object.
+ */
+        static void Main(string[] args)
+        {
+            Builder builder = new ConcreteBuilder();
+            Director director = new Director(builder);
+
+            Console.WriteLine("Standart basic product:\n");
+            director.buildMinimalViableProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            Console.WriteLine("Standart full featured product:\n");
+            director.buildFullFeaturedProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            // Remember, the Builder pattern can be used without a Director class.
+            builder.BuildPartA();
+            builder.BuildPartC();
+            Console.WriteLine(builder.GetProduct().ListParts());
+        }
+    }
+
+    class Director
+    {
+        Builder builder;
+        public Director(Builder builder)
+        {
+            this.builder = builder;
+        }
+        public void buildMinimalViableProduct()
+        {
+            builder.BuildPartA();
+        }
+        public void buildFullFeaturedProduct()
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+            builder.BuildPartC();
+        }
+    }
+
+    abstract class Builder
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract void BuildPartC();
+        public abstract Product GetProduct();
+    }
+
+    class Product
+    {
+        List<object> parts = new List<object>();
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            for (int i = 0; i < parts.Count; i++)
+                str += parts[i] + " | ";
+
+            return "Product parts: " + str + "\n\n";
+        }
+    }
+
+    class ConcreteBuilder : Builder
+    {
+        Product product = new Product();
+        public override void BuildPartA()
+        {
+            product.Add("Part A");
+        }
+        public override void BuildPartB()
+        {
+            product.Add("Part B");
+        }
+        public override void BuildPartC()
+        {
+            product.Add("Part C");
+        }
+
+
+        public override Product GetProduct()
+        {
+            Product result = product;
+
+            this.Reset();
+
+            return result;
+        }
+
+        public void Reset()
+        {
+            product = new Product();
+        }
+    }
+}
---
+++ RefactoringGuru.DesignPatterns.Proxy.Structural/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client: Executing the client code with a real subject:\n");
+
+            RealSubject realSubject = new RealSubject();
+            realSubject.Request();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Client: Executing the same client code with a proxy:\n");
+            Proxy proxy = new Proxy();
+            proxy.Request();
+        }
+    }
+
+    abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("RealSubject: Handling Request.");
+        }
+    }
+    class Proxy : Subject
+    {
+        RealSubject realSubject;
+        public override void Request()
+        {
+            if (this.checkAccess())
+            {
+                realSubject = new RealSubject();
+                realSubject.Request();
+
+                this.logAccess();
+            }
+
+        }
+
+        public bool checkAccess()
+        {
+            Console.WriteLine("Proxy: Checking access prior to firing a real request.");
+
+            return true;
+        }
+        public void logAccess()
+        {
+            Console.WriteLine("Proxy: Logging the time of request.");
+        }
+    }
+}
+
