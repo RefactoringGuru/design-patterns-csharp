d33754b1cca28ac5ed81eccd2cad4bade9d92e97
Alexey Pyltsyn <lex61rus@gmail.com>
2019-02-08 01:28:38 +0300

    Observer: improve conceptual example

--- Observer.Conceptual/Program.cs
+++ Observer.Conceptual/Program.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿// EN: Observer Design Pattern
+//
+// Intent: Define a one-to-many dependency between objects so that when one
+// object changes state, all of its dependents are notified and updated
+// automatically.
+//
+// Note that there's a lot of different terms with similar meaning associated
+// with this pattern. Just remember that the Subject is also called the
+// Publisher and the Observer is often called the Subscriber and vice versa.
+// Also the verbs "observe", "listen" or "track" usually mean the same thing.
+//
+// RU: Паттерн Наблюдатель
+//
+// Назначение: Устанавливает между объектами зависимость «один ко многим» таким
+// образом, что когда изменяется состояние одного объекта, все зависимые от
+// него объекты оповещаются и обновляются автоматически.
+//
+// Обратите внимание, что существует множество различных терминов с похожими
+// значениями, связанных с этим паттерном. Просто помните, что Субъекта также
+// называют Издателем, а Наблюдателя часто называют Подписчиком и наоборот.
+// Также глаголы «наблюдать», «слушать» или «отслеживать» обычно означают одно
+// и то же.
+
+using System;
 using System.Collections.Generic;
 using System.Threading;
 
@@ -6,93 +29,126 @@ namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
 {
     public interface IObserver
     {
-        void update(ISubject subject);
+        // EN: Receive update from subject
+        //
+        // RU: Получает обновление от издателя
+        void Update(ISubject subject);
     }
 
     public interface ISubject
     {
-        void attach(IObserver observer);
+        // EN: Attach an observer to the subject.
+        //
+        // RU: Присоединяет наблюдателя к издателю.
+        void Attach(IObserver observer);
 
-        void detach(IObserver observer);
+        // EN: Detach an observer from the subject.
+        //
+        // RU: Отсоединяет наблюдателя от издателя.
+        void Detach(IObserver observer);
 
-        void notify();
+        // EN: Notify all observers about an event.
+        //
+        // RU: Уведомляет всех наблюдателей о событии.
+        void Notify();
     }
 
+    // EN: The Subject owns some important state and notifies observers when the
+    // state changes.
+    //
+    // RU: Издатель владеет некоторым важным состоянием и оповещает наблюдателей о
+    // его изменениях.
     public class Subject : ISubject
     {
+        // EN: For the sake of simplicity, the Subject's state, essential
+        // to all subscribers, is stored in this variable.
+        //
+        // RU: Для удобства в этой переменной хранится состояние Издателя,
+        // необходимое всем подписчикам.
         public int State { get; set; } = -0;
 
+        // EN: List of subscribers. In real life, the list of subscribers
+        // can be stored more comprehensively (categorized by event type, etc.).
+        //
+        // RU: Список подписчиков. В реальной жизни список подписчиков
+        // может храниться в более подробном виде (классифицируется по типу события
+        // и т.д.)
         private List<IObserver> _observers = new List<IObserver>();
 
-        public void attach(IObserver observer)
+        // EN: The subscription management methods.
+        //
+        // RU: Методы управления подпиской.
+        public void Attach(IObserver observer)
         {
-            Console.Write("Subject: Attached an observer.\n");
+            Console.WriteLine("Subject: Attached an observer.");
             this._observers.Add(observer);
         }
 
-        public void detach(IObserver observer)
+        public void Detach(IObserver observer)
         {
-            foreach (var elem in _observers)
-            {
-                if (elem == observer)
-                {
-                    _observers.Remove(observer);
-                    Console.Write("Subject: Detached an observer.\n");
-                    break;
-                }
-            }
+            this._observers.Remove(observer);
+            Console.WriteLine("Subject: Detached an observer.");
         }
 
-        public void notify()
+        //
+        // EN: Trigger an update in each subscriber.
+        //
+        // RU: Запуск обновления в каждом подписчике.
+        ///
+        public void Notify()
         {
-            Console.Write("Subject: Notifying observers...\n");
+            Console.WriteLine("Subject: Notifying observers...");
 
             foreach (var observer in _observers)
             {
-                observer.update(this);
+                observer.Update(this);
             }
         }
 
-        public void someBusinessLogic()
+        // EN: Usually, the subscription logic is only a fraction of what a Subject
+        // can really do. Subjects commonly hold some important business logic, that
+        // triggers a notification method whenever something important is about to
+        // happen (or after it).
+        //
+        // RU: Обычно логика подписки – только часть того, что делает Издатель.
+        // Издатели часто содержат некоторую важную бизнес-логику, которая запускает
+        // метод уведомления всякий раз, когда должно произойти что-то важное (или
+        // после этого).
+        public void SomeBusinessLogic()
         {
-            Console.Write("\nSubject: I'm doing something important.\n");
+            Console.WriteLine("\nSubject: I'm doing something important.");
             this.State = new Random().Next(0, 10);
 
             Thread.Sleep(15);
 
-            Console.Write("Subject: My state has just changed to: " + this.State + "\n");
-            this.notify();
+            Console.WriteLine("Subject: My state has just changed to: " + this.State);
+            this.Notify();
         }
     }
 
+    // EN: Concrete Observers react to the updates issued by the Subject they had
+    // been attached to.
+    //
+    // RU: Конкретные Наблюдатели реагируют на обновления, выпущенные Издателем,  к
+    // которому они прикреплены.
     class ConcreteObserverA : IObserver
     {
-        public void update(ISubject subject)
-        {
-            if (!(subject is Subject))
-            {
-                return;
-            }
-            
+        public void Update(ISubject subject)
+        {            
             if ((subject as Subject).State < 3)
             {
-                Console.Write("ConcreteObserverA: Reacted to the event.\n");
+                Console.WriteLine("ConcreteObserverA: Reacted to the event.");
             }
         }
     }
 
     class ConcreteObserverB : IObserver
     {
-        public void update(ISubject subject)
+        public void Update(ISubject subject)
         {
-            if (!(subject is Subject))
-            {
-                return;
-            }
-
             if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
             {
-                Console.Write("ConcreteObserverB: Reacted to the event.\n");
+                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
             }
         }
     }
@@ -101,19 +157,22 @@ namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
     {
         public static void ClientCode()
         {
-            var subj = new Subject();
-            var o1 = new ConcreteObserverA();
-            subj.attach(o1);
+            // EN: The client code.
+            //
+            // RU: Клиентский код.
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
 
-            var o2 = new ConcreteObserverB();
-            subj.attach(o2);
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
 
-            subj.someBusinessLogic();
-            subj.someBusinessLogic();
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
 
-            subj.detach(o2);
+            subject.Detach(observerB);
 
-            subj.someBusinessLogic();
+            subject.SomeBusinessLogic();
         }
     }
 
