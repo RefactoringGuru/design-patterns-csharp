c4969e311c27a61125d127b09fd06ef5c34a6cbf
Alexey Pyltsyn <lex61rus@gmail.com>
2019-02-08 01:19:12 +0300

    State: improve conceptual example

--- State.Conceptual/Program.cs
+++ State.Conceptual/Program.cs
@@ -1,83 +1,117 @@
-﻿using System;
+﻿// EN: State Design Pattern
+//
+// Intent: Allow an object to alter its behavior when its internal state
+// changes. The object will appear to change its class.
+//
+// RU: Паттерн Состояние
+//
+// Назначение: Позволяет объекту менять поведение при изменении его внутреннего
+// состояния. Со стороны может казаться, что объект меняет свой класс.
+
+using System;
 
 namespace RefactoringGuru.DesignPatterns.State.Conceptual
 {
+    // EN: The Context defines the interface of interest to clients. It also
+    // maintains a reference to an instance of a State subclass, which represents
+    // the current state of the Context.
+    //
+    // RU: Контекст определяет интерфейс, представляющий интерес для клиентов. Он
+    // также хранит ссылку на экземпляр подкласса Состояния, который отображает
+    // текущее состояние Контекста.
     class Context
     {
+        // EN: A reference to the current state of the Context.
+        //
+        // RU: Ссылка на текущее состояние Контекста.
         private State _state = null;
 
         public Context(State state)
         {
-            this.transitionTo(state);
+            this.TransitionTo(state);
         }
 
-        public void transitionTo(State state)
+        // EN: The Context allows changing the State object at runtime.
+        //
+        // RU: Контекст позволяет изменять объект Состояния во время выполнения.
+        public void TransitionTo(State state)
         {
-            Console.Write("Context: Transition to " + state.ToString() + ".\n");
+            Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
             this._state = state;
-            this._state.setContext(this);
+            this._state.SetContext(this);
         }
 
-        public void request1()
+        // EN: The Context delegates part of its behavior to the current State
+        // object.
+        //
+        // RU: Контекст делегирует часть своего поведения текущему объекту
+        // Состояния.
+        public void Request1()
         {
-            this._state.handle1();
+            this._state.Handle1();
         }
 
-        public void request2()
+        public void Request2()
         {
-            this._state.handle2();
+            this._state.Handle2();
         }
     }
     
+    // EN: The base State class declares methods that all Concrete State should
+    // implement and also provides a backreference to the Context object, associated
+    // with the State. This backreference can be used by States to transition the
+    // Context to another State.
+    //
+    // RU: Базовый класс Состояния объявляет методы, которые должны реализовать все
+    // Конкретные Состояния, а также предоставляет обратную ссылку на объект
+    // Контекст, связанный с Состоянием. Эта обратная ссылка может использоваться
+    // Состояниями для передачи Контекста другому Состоянию.
     abstract class State
     {
-        protected Context context;
+        protected Context _context;
 
-        public void setContext(Context context)
+        public void SetContext(Context context)
         {
-            this.context = context;
+            this._context = context;
         }
 
-        public abstract void handle1();
+        public abstract void Handle1();
 
-        public abstract void handle2();
+        public abstract void Handle2();
     }
 
+    // EN: Concrete States implement various behaviors, associated with a state of
+    // the Context.
+    //
+    // RU: Конкретные Состояния реализуют различные модели поведения, связанные с
+    // состоянием Контекста.
     class ConcreteStateA : State
     {
-        public override void handle1()
+        public override void Handle1()
         {
-            Console.Write("ConcreteStateA handles request1.\n");
-            Console.Write("ConcreteStateA wants to change the state of the context.\n");
-            this.context.transitionTo(new ConcreteStateB());
+            Console.WriteLine("ConcreteStateA handles request1.");
+            Console.WriteLine("ConcreteStateA wants to change the state of the context.");
+            this._context.TransitionTo(new ConcreteStateB());
         }
 
-        public override void handle2()
+        public override void Handle2()
         {
-            Console.Write("ConcreteStateA handles request2.\n");
+            Console.WriteLine("ConcreteStateA handles request2.");
         }
     }
 
     class ConcreteStateB : State
     {
-        public override void handle1()
+        public override void Handle1()
         {
-            Console.Write("ConcreteStateB handles request1.\n");
+            Console.Write("ConcreteStateB handles request1.");
         }
 
-        public override void handle2()
+        public override void Handle2()
         {
-            Console.Write("ConcreteStateB handles request2.\n");
-            Console.Write("ConcreteStateB wants to change the state of the context.\n");
-            this.context.transitionTo(new ConcreteStateA());
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Client.ClientCode();
+            Console.WriteLine("ConcreteStateB handles request2.");
+            Console.WriteLine("ConcreteStateB wants to change the state of the context.");
+            this._context.TransitionTo(new ConcreteStateA());
         }
     }
 
@@ -85,9 +119,20 @@ namespace RefactoringGuru.DesignPatterns.State.Conceptual
     {
         public static void ClientCode()
         {
+            // EN: The client code.
+            //
+            // RU: Клиентский код.
             var context = new Context(new ConcreteStateA());
-            context.request1();
-            context.request2();
+            context.Request1();
+            context.Request2();
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client.ClientCode();
         }
     }
 }
