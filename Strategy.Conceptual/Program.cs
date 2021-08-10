e5378b508702017943ea22d437c6973415439fb2
David Henley <github@drh.dev>
2021-08-10 14:54:08 -0500

    Update Program.cs
    The Context expects a strategy in the constructor. This would not compile without it.

--- Strategy.Conceptual/Program.cs
+++ Strategy.Conceptual/Program.cs
@@ -128,10 +128,9 @@ namespace RefactoringGuru.DesignPatterns.Strategy.Conceptual
             // RU: Клиентский код выбирает конкретную стратегию и передаёт её в
             // контекст. Клиент должен знать о различиях между стратегиями,
             // чтобы сделать правильный выбор.
-            var context = new Context();
+            var context = new Context(new ConcreteStrategyA());
 
             Console.WriteLine("Client: Strategy is set to normal sorting.");
-            context.SetStrategy(new ConcreteStrategyA());
             context.DoSomeBusinessLogic();
             
             Console.WriteLine();
